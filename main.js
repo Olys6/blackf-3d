fe8566656c1a5c9dc9b911c2eb2e1780503c3702
Oliver S <oliver@skjevesland.com>
2022-11-25 18:05:53 +0000

    made planet smaller, made cubes glitch more, added new forms on product titles that change every 3 seconds, added the footer with button and made website responsive on mobile

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 import './style.css'
 
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const products = ["BUYER'S REMORSE", 'THE ILLUSION OF FULFILMENT', "UNRELENTING EXISTENTIAL DREAD", "ANOTHER THING YOU CAN'T TAKE TO THE GRAVE", 'ABSOLUTELY NOTHING']
+let productIndex = 0
+
+setInterval(() => {
+  if (productIndex === products.length) productIndex = 0
+
+  document.getElementById("productTitle").innerText = products[productIndex]
+  productIndex += 1
+
+}, 3300)
 
 const scene = new THREE.Scene();
 
@@ -16,22 +28,68 @@ camera.position.setZ(30);
 
 renderer.render( scene, camera );
 
-const frontBoxGeometry = new THREE.BoxGeometry( 5.5, 5.5, 5.5, 1, 1, 1)
-const frontBoxMaterial = new THREE.MeshStandardMaterial({ color: "white", wireframe: true })
-const frontCube = new THREE.Mesh(frontBoxGeometry, frontBoxMaterial )
-frontCube.position.z = 4
-frontCube.position.y = 2
-
-scene.add(frontCube)
-
 const moonTexture = new THREE.TextureLoader().load("moon wrap.png")
 const moon3dTexture = new THREE.TextureLoader().load("normal.jpg")
-const moonGeometry = new THREE.SphereGeometry(60, 100, 100);
-const moonMaterial = new THREE.MeshBasicMaterial({ color: "gray", map: moonTexture, normalMap: moon3dTexture });
+const moonGeometry = new THREE.SphereGeometry(55, 100, 100);
+const moonMaterial = new THREE.MeshBasicMaterial({ color: "gray", map: moonTexture });
 const moon = new THREE.Mesh( moonGeometry, moonMaterial)
 moon.position.z = -100
 
 scene.add(moon)
+
+const moonCenter = new THREE.Object3D();
+moonCenter.position.z = -100
+moonCenter.rotation.x = 1.5
+scene.add(moonCenter)
+
+
+const boxSize = 14
+const BoxGeometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize, 1, 1, 1)
+
+// material
+const BoxMaterial = new THREE.MeshStandardMaterial({ color: "white", wireframe: true })
+
+let cubeRotation = 2;
+let frontCube = null;
+const numOfCubes = 15;
+
+function addMoonCubes() {
+  frontCube = new THREE.Object3D();
+  frontCube.rotation.z = cubeRotation * Math.PI / numOfCubes;
+  moonCenter.add(frontCube)
+  cubeRotation += 2
+
+  const mesh = new THREE.Mesh(BoxGeometry, BoxMaterial)
+  mesh.position.y = 68
+  // mesh.rotation.z = 1
+  mesh.rotation.x = 0.5
+  frontCube.add(mesh)
+
+  function animate() {
+    requestAnimationFrame(animate);
+    // mesh.rotation.x = 0.6
+    // mesh.rotation.z += 0.05
+    if (mesh.rotation.z < 0.1) {
+      mesh.rotation.z += 0.07
+    } else {
+      mesh.rotation.z -= 0.07
+    }
+    if (mesh.rotation.x < 0.5) {
+      mesh.rotation.x += 0.07
+    } else {
+      mesh.rotation.x -= 0.07
+    }
+  }
+
+  animate()
+
+}
+
+
+Array(numOfCubes).fill().forEach(addMoonCubes)
+
+
+
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.20);
@@ -49,7 +107,7 @@ Array(300).fill().forEach(addStar)
 
 
 const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(0, frontCube.position.y + 5, 20)
+// pointLight.position.set(0, frontCube.position.y + 5, 20)
 
 scene.add(pointLight)
 
@@ -60,20 +118,27 @@ scene.add(ambientLight)
 const lightHelper = new THREE.PointLightHelper(pointLight)
 // scene.add(lightHelper)
 
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.update();
+
 function animate() {
   requestAnimationFrame( animate );
 
-  frontCube.rotation.x = 0.4
-  frontCube.rotation.y += 0.05
-  if (frontCube.rotation.z < 0.1) {
-    frontCube.rotation.z += 0.05
-  } else {
-    frontCube.rotation.z -= 0.05
-  }
+  // controls.update();
+
+  // frontCube.rotation.x = 0.4
+  // frontCube.rotation.y += 0.05
+  // if (frontCube.rotation.z < 0.1) {
+  //   frontCube.rotation.z += 0.05
+  // } else {
+  //   frontCube.rotation.z -= 0.05
+  // }
   // cube.rotation.z += 0.01
 
   moon.rotation.y += 0.005
   moon.rotation.z += 0.005
+
+  moonCenter.rotation.z += 0.005
 
   renderer.render(scene, camera)
 }
